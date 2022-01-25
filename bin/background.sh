20be92cb02742453679ca692c4520928d0ffbbd7
craig <67805796+fallet1@users.noreply.github.com>
2022-01-25 10:53:59 +0000

    No desktop icons

--- bin/background.sh
+++ bin/background.sh
@@ -5,6 +5,7 @@ if [[ "$XDG_CURRENT_DESKTOP" = "MATE" ]]; then
     [ -f ~/Pictures/background ] && gsettings set org.mate.background picture-filename ~/Pictures/background
     [ -d ~/Pictures/background ] && gsettings set org.mate.background picture-filename \
 	"${HOME}/Pictures/background/$(/bin/ls -1 ${HOME}/Pictures/background | sort -R | head -n 1)"
+    gsettings set org.mate.background show-desktop-icons false
 fi
 
 if [[ "$XDG_CURRENT_DESKTOP" = "i3" ]]; then
