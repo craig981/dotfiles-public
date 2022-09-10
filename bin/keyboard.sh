19fe94924e70938678e09fa880d5aa63596436ff
craig <67805796+acgir@users.noreply.github.com>
2022-09-10 15:52:02 +0100

    Config for external keyboard, remove annoying MATE bindings

--- bin/keyboard.sh
+++ bin/keyboard.sh
@@ -8,14 +8,21 @@ setxkbmap -option caps:ctrl_modifier
 setxkbmap -option ctrl:ralt_rctrl
 setxkbmap -option altwin:menu_win
 
-if [[ "$XDG_CURRENT_DESKTOP" = "MATE" ]]; then
-    gsettings set org.mate.interface gtk-key-theme Emacs
+id=$(xinput list | sed -e '1,/Virtual core keyboard/d' | grep -m 1 Keychron | sed -e 's/.*id=\([0-9]\+\).*/\1/')
+if [[ -n "${id}" ]]; then
+	setxkbmap -device "${id}" -option
+	setxkbmap -device "${id}" -option caps:ctrl_modifier
+	setxkbmap -device "${id}" -option ctrl:swap_rwin_rctl
+	setxkbmap -device "${id}" -option altwin:swap_lalt_lwin
 fi
 
-# # mac keyboard
-# setxkbmap -option
-# setxkbmap -option caps:ctrl_modifier
-# setxkbmap -option ctrl:swap_rwin_rctl
-# setxkbmap -option altwin:swap_lalt_lwin
+if [[ "$XDG_CURRENT_DESKTOP" = "MATE" ]]; then
+    gsettings set org.mate.interface gtk-key-theme Emacs
+    # disable Super_L key opening menu
+    gsettings set org.mate.mate-menu hot-key ''
+    gsettings set com.solus-project.brisk-menu hot-key ''
+    # disable Super_L-E opening file explorer
+    gsettings set org.mate.Marco.global-keybindings run-command-3 ''
+fi
 
 # https://docs.microsoft.com/en-us/sysinternals/downloads/ctrl2cap
