13016819fe1412ab2ad789a03202e2f8e99f41c7
craig <67805796+acgir@users.noreply.github.com>
2022-10-01 21:42:41 +0100

    Add ef-night

--- common/.emacs.d/my-override-dark2-theme.el
+++ common/.emacs.d/my-override-dark2-theme.el
@@ -3,11 +3,11 @@
 
 (custom-theme-set-faces
  'my-override-dark2
- '(fringe ((t (:background nil))))
- '(isearch ((t (:background "gold3" :foreground "black"))))
- '(lazy-highlight ((t (:background "turquoise" :foreground "black" ))))
- '(helm-match ((t (:inherit region))))
- '(show-paren-match ((t (:weight bold :background nil :foreground "#ffffff"))))
+ ;; '(fringe ((t (:background nil))))
+ ;; '(isearch ((t (:background "gold3" :foreground "black"))))
+ ;; '(lazy-highlight ((t (:background "turquoise" :foreground "black" ))))
+ '(helm-match ((t (:foreground "#ffffff" :background "#005f5f"))))
+ ;; '(show-paren-match ((t (:weight bold :background nil :foreground "#ffffff"))))
 )
 
 (provide-theme 'my-override-dark2)
