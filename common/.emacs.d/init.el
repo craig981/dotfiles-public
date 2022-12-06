a9cf7cf4cc17d8a5c5659d2872b5156bbc4da690
craig <67805796+acgir@users.noreply.github.com>
2022-12-06 22:28:03 +0000

    Remove -k from default compile command

--- common/.emacs.d/init.el
+++ common/.emacs.d/init.el
@@ -1506,6 +1506,7 @@ return the project path instead"
 ;* Compilation
 ;; ----------------------------------------------------------------------------
 
+(setq-default compile-command "make")
 (setq-default compilation-ask-about-save nil)
 (setq-default compilation-scroll-output t)
 (setq-default compilation-skip-threshold 2) ;; skip warnings
