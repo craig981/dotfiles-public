845668461dda5174a2a02136f76bd01f1e4fe97d
craig <67805796+acgir@users.noreply.github.com>
2022-12-29 15:42:58 +0000

    Org todo keywords

--- common/.emacs.d/init.el
+++ common/.emacs.d/init.el
@@ -658,8 +658,9 @@
 (setq org-src-fontify-natively t)
 (setq org-startup-folded nil)
 (setq org-confirm-babel-evaluate nil)
-;; (when (eq system-type 'darwin)
-;;   (setq org-todo-keywords '((sequence "TODO" "WAITING" "DONE"))))
+(when (eq system-type 'gnu/linux)
+  (setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)")
+			    (sequence "|" "CANCELLED(c)"))))
 
 (setq org-capture-templates
       '(("b" "Bookmark" entry (file+headline org-default-notes-file "Bookmarks")
