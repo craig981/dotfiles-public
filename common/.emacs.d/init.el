64ea7edc93aa6ef51572f086be64cf46b3907abb
craig <67805796+acgir@users.noreply.github.com>
2023-01-05 12:39:02 +0000

    Change winner-undo binding, conflicts with backtab in terminal

--- common/.emacs.d/init.el
+++ common/.emacs.d/init.el
@@ -884,8 +884,8 @@
 ;; ----------------------------------------------------------------------------
 
 (winner-mode 1)
-(global-set-key (kbd "M-[") 'winner-undo)
-(global-set-key (kbd "M-]") 'winner-redo)
+(global-set-key (kbd "C-c h") 'winner-undo)
+(global-set-key (kbd "C-c H") 'winner-redo)
 
 ;; ----------------------------------------------------------------------------
 ;* Vertico, orderless, marginalia
