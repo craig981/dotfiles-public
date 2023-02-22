32c78caef08834c6bd8ccb1eea032ebb8e160bd3
craig <67805796+acgir@users.noreply.github.com>
2023-02-22 17:53:21 +0000

    Run ledger on encrypted file

---
+++ bin/ledger.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# run ledger on encrypted file
+
+# edit arguments, replacing file with stdin
+new_args=()
+skip=0
+file=
+for a in "$@"; do
+    if [[ "${a}" == "-f" ]]; then
+	new_args+=("-f")
+	new_args+=("-")
+	skip=2
+    fi
+    if (( "${skip}" )); then
+	if (( ${skip} == 1 )); then
+	    file="${a}"
+	fi
+	skip=$(( ${skip} - 1 ));
+    else
+	new_args+=("${a}")
+    fi
+done
+
+if [[ -z "${file}" ]]; then
+    echo "failed to find file in ledger argument list"
+    exit 1
+fi
+
+set -xe
+gpg2 --decrypt "${file}" | /opt/local/bin/ledger "${new_args[@]}"
