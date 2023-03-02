3883ebb15d2fa0950e4b88114eac2779253c9be4
craig <67805796+acgir@users.noreply.github.com>
2023-03-02 23:33:02 +0000

    Remove marginalia-align override

--- common/.emacs.d/init.el
+++ common/.emacs.d/init.el
@@ -2519,7 +2519,6 @@ current project instead. Visit the tags file."
  '(magit-section-visibility-indicator '("" . t))
  '(magit-status-headers-hook
    '(magit-insert-error-header magit-insert-diff-filter-header magit-insert-repo-header magit-insert-head-branch-header magit-insert-upstream-branch-header magit-insert-push-branch-header magit-insert-tags-header))
- '(marginalia-align 'center)
  '(org-agenda-show-future-repeats nil)
  '(org-blank-before-new-entry '((heading . auto) (plain-list-item)))
  '(org-imenu-depth 3)
