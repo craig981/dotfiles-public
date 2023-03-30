92d433c498c7c403e6c346d856a6d3a2880bebed
craig <67805796+acgir@users.noreply.github.com>
2023-03-30 11:22:42 +0100

    Resolve symlink before passing to gsettings
    Seems to cache and it's not invalidated by changing the symlink

--- bin/background.sh
+++ bin/background.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+img=$(readlink -m ~/Pictures/background)
 
 if [[ "$XDG_CURRENT_DESKTOP" = "MATE" ]]; then
-    [ -f ~/Pictures/background ] && gsettings set org.mate.background picture-filename ~/Pictures/background
-    [ -d ~/Pictures/background ] && gsettings set org.mate.background picture-filename \
+    [ -f "${img}" ] && gsettings set org.mate.background picture-filename "${img}"
+    [ -d "${img}" ] && gsettings set org.mate.background picture-filename \
 	"${HOME}/Pictures/background/$(/bin/ls -1 ${HOME}/Pictures/background | sort -R | head -n 1)"
     gsettings set org.mate.background show-desktop-icons false
     gsettings set org.mate.background picture-options zoom # or centered
