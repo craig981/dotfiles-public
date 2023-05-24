55d71b29194254219dbbd4ea5aec2d38a0ba01c0
craig <67805796+acgir@users.noreply.github.com>
2023-05-24 19:27:16 +0100

    Change theme

--- emacs/.emacs.d/my-override-light2-theme.el
+++ emacs/.emacs.d/my-override-light2-theme.el
@@ -3,11 +3,17 @@
 
 (custom-theme-set-faces
  'my-override-light2
- '(default ((t (:background "#F0F0F6"))))
+ ;; '(default ((t (:background "#F0F0F6"))))
+
+ ;; swap removed and added faces from anti-zenburn
+ '(diff-refine-removed ((t (:foreground "#502750" :background "#c0a0c0"))))
+ '(diff-refine-added ((t (:foreground "#134c4c" :background "#83bcbc"))))
+ '(diff-removed ((t (:foreground "#502750" :background "#c0a0c0"))))
+ '(diff-added ((t (:foreground "#134c4c" :background "#83bcbc"))))
  )
 
 (set-cursor-color "black")
 (setq evil-normal-state-cursor '(box "black"))
-(setq evil-insert-state-cursor '(box "orange"))
+(setq evil-insert-state-cursor '(box "white"))
 
 (provide-theme 'my-override-light2)
