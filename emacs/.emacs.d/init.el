1ac7eceb3edf0bfe11eed70828aa0dd6ce0c744f
craig <67805796+acgir@users.noreply.github.com>
2023-08-10 22:02:08 +0100

    Fall through to emacs key binding for transpose chars

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -114,6 +114,7 @@ leave it at 't' for Emacs commands"
 (define-key evil-normal-state-map (kbd "M-.") nil)
 (define-key evil-normal-state-map (kbd "M-,") nil)
 (define-key evil-visual-state-map (kbd "C-w") 'kill-region)
+(evil-global-set-key 'normal (kbd "C-t") nil)
 (evil-global-set-key 'motion (kbd "C-a") nil)
 (evil-global-set-key 'insert (kbd "C-a") nil)
 (evil-global-set-key 'insert (kbd "C-e") nil)
