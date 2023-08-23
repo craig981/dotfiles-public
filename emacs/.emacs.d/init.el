ed45e733513f303aa47fa1177a385ff08649c8ef
craig <67805796+acgir@users.noreply.github.com>
2023-08-23 20:30:11 +0100

    Rebind

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1735,7 +1735,7 @@ return the project path instead"
 
 (add-hook 'sh-mode-hook 'my-syntax-entry)
 
-(global-set-key (kbd "C-c M-v") 'my-shell)
+(global-set-key (kbd "C-c i") 'my-shell)
 (global-set-key (kbd "C-c v") 'my-split-shell)
 (global-set-key (kbd "C-c h") 'my-jump-to-shell)
 
