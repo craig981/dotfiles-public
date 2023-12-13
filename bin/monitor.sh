9dcce036fe5714cac5646e72c6281178a4100697
craig <67805796+crai9d@users.noreply.github.com>
2023-12-13 21:00:11 +0000

    Chrome notes

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -8,8 +8,13 @@ fi
 #     brightnessctl -d acpi_video0 s 8
 # fi
 
-# Chrome fuzzy/soft font rendering.
-# Open "chrome://flags", search "ozone-platform-hint", set to Auto.
+# Chrome
+#
+# Fix fuzzy/soft font rendering.
+# chrome://flags/#ozone-platform-hint Auto
+#
+# Remove drop-down menu at the left of the tab bar.
+# chrome://flags/#customize-chrome-side-panel Disable
 
 # switch off laptop screen when external monitor connected
 #[[ $(xrandr | awk '/^HDMI-1/{print $2}') == "connected" ]] && xrandr --output eDP-1 --off
