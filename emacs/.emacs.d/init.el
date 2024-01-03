d107df92d4435196431035985f95a14fa41e621c
craig <67805796+crai9d@users.noreply.github.com>
2024-01-03 17:19:50 +0000

    Remove ledger --force-color arg

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2602,6 +2602,7 @@ current project instead, and visit the tags file."
  '(hide-ifdef-shadow t)
  '(initial-frame-alist '((fullscreen . maximized)))
  '(ispell-program-name "aspell")
+ '(ledger-report-native-highlighting-arguments '("--color"))
  '(magit-log-arguments '("--graph" "--color" "--decorate" "-n256"))
  '(magit-log-auto-more t)
  '(magit-merge-arguments '("--no-ff"))
