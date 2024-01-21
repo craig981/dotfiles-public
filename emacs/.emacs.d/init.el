291b5bdffa7fb04a1aef68c9d7dc39283539fa3e
craig <67805796+crai9d@users.noreply.github.com>
2024-01-21 16:00:54 +0000

    Bind magit-section-up

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1655,6 +1655,7 @@ return the project path instead"
   (add-hook 'magit-mode-hook #'my-magit-hook)
 
   (evil-define-key 'normal magit-mode-map (kbd "<escape>") nil) ;; stop escape burying buffer
+  (evil-define-key 'normal magit-mode-map (kbd ";") 'magit-section-up)
   (evil-define-key 'normal magit-mode-map (kbd "p") 'magit-section-backward)
   (evil-define-key 'normal magit-mode-map (kbd "n") 'magit-section-forward))
 
