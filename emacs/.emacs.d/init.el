8f117aaedf20caada480bd6b767942e96c5755cb
craig <67805796+cseivad@users.noreply.github.com>
2024-04-14 20:05:41 +0100

    Fixed broken copy for floats

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1134,12 +1134,9 @@ empty string."
 ;;| Calc
 ;; ----------------------------------------------------------------------------
 
-(defun my-calc-copy (&optional prefix)
-  (interactive "P")
-  (let* ((v (calc-top))
-	 (x (if prefix
-		(math-format-number v)
-	      (format "%s" v))))
+(defun my-calc-copy ()
+  (interactive)
+  (let ((x (math-format-number (calc-top))))
     (kill-new x)
     (message "Yanked %s" x)))
 
