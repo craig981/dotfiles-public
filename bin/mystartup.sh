ceaf6c8bd263fcff6af4c8cdfb298f69dc3e9220
craig <67805796+cseivad@users.noreply.github.com>
2024-09-04 16:17:36 +0100

    Clear show-hidden in file browser

--- bin/mystartup.sh
+++ bin/mystartup.sh
@@ -9,4 +9,5 @@ sleep 2
 
 if [[ "$XDG_CURRENT_DESKTOP" = "ubuntu:GNOME" ]]; then
     gsettings set org.gnome.desktop.privacy remember-recent-files false
+    gsettings set org.gtk.Settings.FileChooser show-hidden false
 fi
