b504312b1b64e6e9a81967c3a6f6fa8ba55a7be4
craig <67805796+cseivad@users.noreply.github.com>
2024-09-20 20:36:59 +0100

    Default to evil mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -305,10 +305,13 @@
 (defun my-find-file-hook ()
   (if (file-remote-p (buffer-file-name))
       (setq-local vc-handled-backends nil))
+
+  (when (not (eq major-mode 'image-mode))
+    (evil-local-mode 1)) ;; default to evil mode
   (when git-commit-mode
-    (evil-local-mode 1)
     (when (and (evil-normal-state-p) (looking-at "^$"))
       (evil-insert-state)))
+
   (if my-input-method
       (set-input-method my-input-method)))
 
