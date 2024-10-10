f92d7d1c160568f54afa006d2aec53a15092ee99
craig <67805796+cseivad@users.noreply.github.com>
2024-10-10 22:10:51 +0100

    Indentation to spaces

--- bin/trackpad.sh
+++ bin/trackpad.sh
@@ -13,12 +13,12 @@ fi
 if [[ "$(hostname -s)" = "asusbox" ]]; then
     id=$(xinput list | awk '/Touchpad/{print $(NF-3)}' | cut -d= -f2)
     if [[ -n "${id}" ]]; then
-	# disable touchpad when mouse plugged in
-	if [[ $(xinput list | grep -i mouse) ]]; then
-	    xinput disable "${id}"
-	else
-	    xinput enable "${id}"
-	fi
+        # disable touchpad when mouse plugged in
+        if [[ $(xinput list | grep -i mouse) ]]; then
+            xinput disable "${id}"
+        else
+            xinput enable "${id}"
+        fi
     fi
 fi
 
