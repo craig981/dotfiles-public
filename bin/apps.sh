6a75a3eaadc5d77f866f80c611405126df3009f8
craig <67805796+cseivad@users.noreply.github.com>
2024-10-10 23:28:47 +0100

    Comment

--- bin/apps.sh
+++ bin/apps.sh
@@ -4,12 +4,12 @@ if [[ "$XDG_CURRENT_DESKTOP" = "ubuntu:GNOME" ]]; then
     gsettings set org.gnome.desktop.privacy remember-recent-files false
     gsettings set org.gtk.Settings.FileChooser show-hidden false
 
-    # make gnome-terminal open tmux
-    if command -v tmux > /dev/null 2>&1; then
-        key=$(~/dotfiles-public/bin/term_profile.sh)
-        if [[ $? -eq 0 && -n "${key}" ]]; then
-            dconf write "${key}/use-custom-command" true
-            dconf write "${key}/custom-command" "'tmux'"
-        fi
-    fi
+    # # make gnome-terminal open tmux
+    # if command -v tmux > /dev/null 2>&1; then
+    #     key=$(~/dotfiles-public/bin/term_profile.sh)
+    #     if [[ $? -eq 0 && -n "${key}" ]]; then
+    #         dconf write "${key}/use-custom-command" true
+    #         dconf write "${key}/custom-command" "'tmux'"
+    #     fi
+    # fi
 fi
