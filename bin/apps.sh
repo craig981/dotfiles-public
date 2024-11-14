abfd8d2f4e85435c234c92922b15333665615dd1
craig <67805796+cseivad@users.noreply.github.com>
2024-11-14 15:19:32 +0000

    No terminal bell noise

--- bin/apps.sh
+++ bin/apps.sh
@@ -4,6 +4,15 @@ if [[ "$XDG_CURRENT_DESKTOP" = "ubuntu:GNOME" ]]; then
     gsettings set org.gnome.desktop.privacy remember-recent-files false
     gsettings set org.gtk.Settings.FileChooser show-hidden false
 
+
+    # no terminal bell noise
+    profile=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")
+    if [[ -n "${profile}" ]]; then
+        key="/org/gnome/terminal/legacy/profiles:/:${profile}"
+        dconf write "${key}/audible-bell" false
+    fi
+
+
     # # make gnome-terminal open tmux
     # if command -v tmux > /dev/null 2>&1; then
     #     key=$(~/dotfiles-public/bin/term_profile.sh)
