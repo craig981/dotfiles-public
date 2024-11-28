bd0469105388af7ceb728c42a026e07535c2c397
craig <67805796+cseivad@users.noreply.github.com>
2024-11-28 20:35:09 +0000

    Prot's better C-g

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -775,7 +775,33 @@ copy the basename."
 (define-key minibuffer-local-map (kbd "<escape>") 'abort-minibuffers)
 
 ;;; stop accidentally moving out of the minibuffer
-(define-key minibuffer-local-map (kbd "M-o") (lambda () (interactive)))
+;; (define-key minibuffer-local-map (kbd "M-o") (lambda () (interactive)))
+
+(defun prot/keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+ The generic `keyboard-quit' does not do the expected thing when
+ the minibuffer is open.  Whereas we want it to close the
+ minibuffer, even without explicitly focusing it.
+
+ The DWIM behaviour of this command is as follows:
+
+ - When the region is active, disable it.
+ - When a minibuffer is open, but not focused, close the minibuffer.
+ - When the Completions buffer is selected, close it.
+ - In every other case use the regular `keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
+
+(define-key global-map (kbd "C-g") #'prot/keyboard-quit-dwim)
 
 ;; ----------------------------------------------------------------------------
 ;;| Olivetti
