471bcdb225f6a84f3a1bd24cf908ffaf26f38fa4
craig <67805796+cseivad@users.noreply.github.com>
2024-12-01 13:44:56 +0000

    Scratch buffer mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -79,7 +79,6 @@
  '(image-dired-thumb-width 100)
  '(inhibit-startup-screen t)
  '(initial-frame-alist '((fullscreen . maximized)))
- '(initial-major-mode 'text-mode)
  '(initial-scratch-message nil)
  '(ispell-program-name "aspell")
  '(kill-ring-max 1000)
