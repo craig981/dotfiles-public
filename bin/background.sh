7b518adfe8a1ad6252cae08d8446e12a06c01099
craig <67805796+cseivad@users.noreply.github.com>
2024-12-06 17:08:36 +0000

    Reindent

--- bin/background.sh
+++ bin/background.sh
@@ -5,7 +5,7 @@ img=$(readlink -m ~/Pictures/background)
 if [[ "$XDG_CURRENT_DESKTOP" = "MATE" ]]; then
     [ -f "${img}" ] && gsettings set org.mate.background picture-filename "${img}"
     [ -d "${img}" ] && gsettings set org.mate.background picture-filename \
-	"${HOME}/Pictures/background/$(/bin/ls -1 ${HOME}/Pictures/background | sort -R | head -n 1)"
+                                 "${HOME}/Pictures/background/$(/bin/ls -1 ${HOME}/Pictures/background | sort -R | head -n 1)"
     gsettings set org.mate.background show-desktop-icons false
     gsettings set org.mate.background picture-options zoom # or centered
 fi
