9e71fafb9497e37ccd1c3a58e9c13ebdff85784d
craig <67805796+cseivad@users.noreply.github.com>
2024-12-06 17:16:13 +0000

    Move background set

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -32,6 +32,34 @@ fi
 #     brightnessctl -d acpi_video0 s 8
 # fi
 
+# switch off laptop screen when external monitor connected
+#[[ $(xrandr | awk '/^HDMI-1/{print $2}') == "connected" ]] && xrandr --output eDP-1 --off
+
+
+
+# background
+
+if [[ "$XDG_CURRENT_DESKTOP" = "ubuntu:GNOME" ]]; then
+    gsettings set org.gnome.desktop.background picture-options 'zoom'
+    # gsettings set org.gnome.desktop.background picture-options 'scaled'
+fi
+
+if [[ "$XDG_CURRENT_DESKTOP" = "MATE" ]]; then
+    img=$(readlink -m ~/Pictures/background)
+    [ -f "${img}" ] && gsettings set org.mate.background picture-filename "${img}"
+    [ -d "${img}" ] && gsettings set org.mate.background picture-filename \
+                                 "${HOME}/Pictures/background/$(/bin/ls -1 ${HOME}/Pictures/background | sort -R | head -n 1)"
+    gsettings set org.mate.background show-desktop-icons false
+    gsettings set org.mate.background picture-options zoom # or centered
+fi
+
+if [[ "$XDG_CURRENT_DESKTOP" = "i3" ]]; then
+    [ -f ~/Pictures/background ] && feh --no-fehbg --bg-fill ~/Pictures/background
+    [ -d ~/Pictures/background ] && feh --no-fehbg --bg-fill -z ~/Pictures/background
+fi
+
+
+
 # Chrome
 #
 # Fix fuzzy/soft font rendering.
@@ -40,5 +68,3 @@ fi
 # Remove drop-down menu at the left of the tab bar.
 # chrome://flags/#customize-chrome-side-panel Disable
 
-# switch off laptop screen when external monitor connected
-#[[ $(xrandr | awk '/^HDMI-1/{print $2}') == "connected" ]] && xrandr --output eDP-1 --off
