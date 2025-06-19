14ab7d1ccc31f9ca4418248f52923301a4917ce3
craig <67805796+cseivad@users.noreply.github.com>
2025-06-19 15:05:11 +0100

    Rewrite shell jump

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2533,13 +2533,24 @@ return key from pasting the whole lot back and executing it."
 	(string-match-p "^\\*gud\\*$"		     name)
 	(string-match-p "^\\*gud-.*\\*$"	     name))))
 
-(defun my-jump-to-shell (&optional other)
+(defun my-jump-to-shell (&optional prefix)
+  "Select successive windows holding shells. With prefix,
+cycle through shells in the current window."
   (interactive "P")
-  (if-let ((target (car (match-buffers 'my-match-shell-predicate))))
-      (if other
-	  (switch-to-buffer-other-window target)
-	(switch-to-buffer target))
-    (message "No shell to jump to")))
+  (let ((buffers (match-buffers 'my-match-shell-predicate)))
+    (if prefix
+	(if-let ((target (car buffers)))
+	  (switch-to-buffer target)
+	(message "No shell to jump to"))
+
+      (let ((target))
+	  (dolist (b buffers)
+	    (when (not target)
+	      (if-let ((w (get-buffer-window b)))
+		  (setq target w))))
+	  (if target
+	      (select-window target)
+	    (message "No window displaying shell to jump to"))))))
 
 (defun my-choose-shell (&optional other)
   "Complete the name and/or default-directory of a shell buffer and
