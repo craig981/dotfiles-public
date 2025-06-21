46e27a5d2df83f0af9f4dbf417ec8c98be0b7668
craig <67805796+cseivad@users.noreply.github.com>
2025-06-21 21:28:30 +0100

    Jumps

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3151,7 +3151,7 @@ to its default value. Leave it alone!"
 
 (defhydra my-jump-hydra (:hint nil)
   "
-_i_: init.el         _s_:   project shell     _b_: ibuffer      _SPC_: agenda               _g_: EMMS
+_i_: init.el         _s_:   project shell     _b_: ibuffer      _SPC_: agenda               _g_: EMMS             _._: dotfiles-public
 _n_: notes           _h_:   shell             _c_: calc         _C-c_: calc other window    _a_: Add playlist
 _w_: world clock     _e_:   jump to shell     _d_: calendar     _v_:   magit list repos     _p_: Playlist new
 _t_: term            _C-e_: choose shell      _r_: scratch      _f_:   ledger               _o_: olivetti
@@ -3163,6 +3163,7 @@ _t_: term            _C-e_: choose shell      _r_: scratch      _f_:   ledger   
   ("s"	 #'my-project-shell      :exit t)
   ("t"	 #'my-term	         :exit t)
   ("v"	 #'my-magit-list-repos   :exit t)
+  ("."	 (lambda () (interactive) (magit "~/dotfiles-public"))   :exit t)
   ("d"	 #'my-project-dired      :exit t)
   ("c"   (lambda () (interactive) (calc nil calc-full-mode t)) :exit t)
   ("C-c" (lambda () (interactive) (calc-other-window t))       :exit t)
